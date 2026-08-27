973b0f4988b0f2be386f609ab2afa227db71fdb0
Rony10-az <ronnjhost@gmail.com>
2026-08-27 12:25:18 -0500

    Ingreso de Datos

--- SEMANA02/Ejercicio02.swift
+++ SEMANA02/Ejercicio02.swift
@@ -1,11 +1,3 @@
-//
-//  Ejercicio02.swift
-//  
-//
-//  Created by Tecsup on 27/08/26.
-//
-
-
 
 import Foundation
 //Framework de swift para el uso de comnados
@@ -18,15 +10,20 @@ let trabajo = 0.3
 
 let ExFinal = 0.4
 
-// commit ingreso de datos
+//Ingreso de datos
+
 print("Nombre del Alumno ")
 let alumno = readLine() ?? " "
 
 print ("Nota del Examen Parcial : ")
 let ExParcial = Double readLine() ?? " ") ?? 0.0
 
+// Nota del examen final
+print("Nota del Examen Final:")
+let notaFinal = Double(readLine() ?? "") ?? 0.0
 
 // commit del calculo
+
 // commit de mostrar los datos
 
 
