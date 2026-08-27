ba3fe5b0331a20d7b6d4d65efcd5b61ae02bbdfc
Rony10-az <ronnjhost@gmail.com>
2026-08-27 12:13:15 -0500

    Nuevos ejercicio  Definir promedio por pesos , Ingreso de Datos

---
+++ SEMANA02/Ejercicio02.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+//Framework de swift para el uso de comnados
+print("Numero 1")
+let a = Double(readLine() ?? "") ?? 0
+print("Numero 2")
+let b = Double(readLine() ?? "") ?? 0
+
+let suma = a + b
+print("La suma es: \(suma)")
