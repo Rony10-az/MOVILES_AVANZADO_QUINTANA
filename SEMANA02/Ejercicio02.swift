4584706b705ffcacd1fa6692c4fb66ea7d11c1df
Rony10-az <ronnjhost@gmail.com>
2026-08-27 12:22:37 -0500

    Nuevo Ejercicio Definir promedio por pesos : ingreso de datos

--- SEMANA02/Ejercicio02.swift
+++ SEMANA02/Ejercicio02.swift
@@ -1,10 +1,33 @@
+//
+//  Ejercicio02.swift
+//  
+//
+//  Created by Tecsup on 27/08/26.
+//
+
+
 
 import Foundation
 //Framework de swift para el uso de comnados
-print("Numero 1")
-let a = Double(readLine() ?? "") ?? 0
-print("Numero 2")
-let b = Double(readLine() ?? "") ?? 0
 
-let suma = a + b
-print("La suma es: \(suma)")
+// definir el promedio de pesos
+
+let ExParcial = 0.3
+
+let trabajo = 0.3
+
+let ExFinal = 0.4
+
+// commit ingreso de datos
+print("Nombre del Alumno ")
+let alumno = readLine() ?? " "
+
+print ("Nota del Examen Parcial : ")
+let ExParcial = Double readLine() ?? " ") ?? 0.0
+
+
+// commit del calculo
+// commit de mostrar los datos
+
+
+
